750326235ce090e1dd044f7b9cc1ea5f182f8450
Javad Bayat <javad78905@gmail.com>
2024-09-30 18:21:09 +0330

    Update foo.sh

--- foo.sh
+++ foo.sh
@@ -1,4 +1,4 @@
 clear
-echo -en "\e[1;34m You no longer need your VPN! Please turn it off and press Enter to continue installation. \e[0m"
+printf "\e[1;34m You no longer need your VPN! Please turn it off and press Enter to continue installation. \e[0m"
 read
 clear
