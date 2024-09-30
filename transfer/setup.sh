b8902a3ce5654b820300ca23b775755ef4572300
Javad Bayat <javad78905@gmail.com>
2024-10-01 01:57:52 +0330

    Update setup.sh

--- transfer/setup.sh
+++ transfer/setup.sh
@@ -10,7 +10,7 @@ curl https://javadbayat.github.io/Test/transfer/transfer.cpp -o transfer.cpp
 mkdir bin
 curl https://javadbayat.github.io/Test/transfer/termux-file-editor -o bin/termux-file-editor
 clang++ transfer.cpp -o bin/transfer
-curl https://javadbayat.github.io/Test/transfer/shortcut.sh >> ~/.bashrc
+echo ~/bin/transfer >> ~/.bashrc
 
 clear
 printf "Installation completed!\n"
